8efd9961d1868eec17d9e230e9d59776fbff9445
lancego2023 <[lancego2023@u.northwestern.edu]>
2021-11-14 16:57:23 -0600

    extend

--- ce361_CPU_Verilog/branch_pc.v
+++ ce361_CPU_Verilog/branch_pc.v
@@ -1,11 +1,15 @@
 // do shit here idk man
+// needs adders, mux, extender
+`include "lib/sram.v"
+`include "lib/syncram.v"
 
-
-module pc_branch(A, imm16, nPC_sel, pc_out);
-   input [31:0] A;
+module pc_branch(pc_in, imm16, nPC_sel, pc_out);
+   input [31:0] pc_in;
    input [15:0] imm16;
    input 	nPC_sel;
    output [31:0] pc_out;
+
+   
 
    
 
---
+++ ce361_CPU_Verilog/extend.v
@@ -0,0 +1,13 @@
+// sign and zero extend.
+`include "lib/and_gate.v"
+
+module extender(in, ext, out);
+   input [15:0] in;
+   input 	ext;
+   output [31:0] out;
+   wire 	 sign;
+
+   and_gate si(.x(ext), .y(in[15]), .z(sign));
+   assign out = {{16{sign}},{in[15:0]}};
+
+endmodule // extender

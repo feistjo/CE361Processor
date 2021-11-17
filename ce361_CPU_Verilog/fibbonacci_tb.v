89a9544012187526db7fbfc21d34d8658736332e
feistjo <joshua.feist@gmail.com>
2021-11-17 16:38:57 -0600

    fibbonacci

--- ce361_CPU_Verilog/fibbonacci_tb.v
+++ ce361_CPU_Verilog/fibbonacci_tb.v
@@ -24,12 +24,11 @@ module fibbonacci_tb();
 			
 			//fibbonacci
 			integer i;
-			for (i = 0; i < n; i = i + 1) begin
-				if (i % 2 == 0) begin
+			for (i = 0; i < n; i = i + 1)
+				if (i % 2 == 0)
 					towrite = 5'b00001;
-				end else begin
+				else
 					towrite = 5'b00010;
-				end
 				Inst = {16'b000000_00001_00001, towrite, 11'b00000_100000}; //add $1, $1, $1
 				clk = 1'b0;
 				#1

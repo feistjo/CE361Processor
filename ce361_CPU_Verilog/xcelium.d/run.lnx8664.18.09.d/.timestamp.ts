f97e31275ae3c1969861f4ce53a633a59276d094
feistjo <joshua.feist@gmail.com>
2021-11-17 13:01:06 -0600

    Revert "deleted xcelium stuff"
    This reverts commit 18886543773a79c4e3c9b7c7d6e4325601371365.

---
+++ ce361_CPU_Verilog/xcelium.d/run.lnx8664.18.09.d/.timestamp.ts
@@ -0,0 +1,7 @@
+1637129391 /home/lcg3895/ce361/CE361SingleCycleProcessor/ce361_CPU_Verilog/test/sram_tb.v
+1637019753 /home/lcg3895/ce361/CE361SingleCycleProcessor/ce361_CPU_Verilog/cpu.v
+1637019753 /home/lcg3895/ce361/CE361SingleCycleProcessor/ce361_CPU_Verilog/extend.v
+1637126062 /home/lcg3895/ce361/CE361SingleCycleProcessor/ce361_CPU_Verilog/lib/sram.v
+1637132864 /home/lcg3895/ce361/CE361SingleCycleProcessor/ce361_CPU_Verilog/pc_tb.v
+1637134877 /home/lcg3895/ce361/CE361SingleCycleProcessor/ce361_CPU_Verilog/branch_pc.v
+1637119882 /home/lcg3895/ce361/CE361SingleCycleProcessor/ce361_CPU_Verilog/registers.v

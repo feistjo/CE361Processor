d4b9f7f229cbc4981f6fc50c365336e43b4264c5
feistjo <joshua.feist@gmail.com>
2021-11-17 00:39:13 -0600

    control_tb

---
+++ ce361_CPU_Verilog/control_tb.v
@@ -0,0 +1,20 @@
+  module control_tb();
+	reg [5:0] Op, Fun;
+	wire equal, sign, nPC_sel, RegWr, RegDst, ExtOp, MemWr, ALUSrc, MemToReg;
+	wire [2:0] ALUctr;
+	
+	control controls(.Op(Op), .Fun(Fun), .equal(equal), .sign(sign), .nPC_sel(nPC_sel), .RegWr(RegWr), .RegDst(RegDst), .ExtOp(ExtOp), .ALUSrc(ALUSrc), .ALUctr(ALUctr), .MemWr(MemWr), .MemtoReg(MemToReg));
+	
+	initial
+		begin
+			//$monitor(Op, Fun, func, RegDst);
+			Op = 6'b0;
+			Fun = 6'b100000;
+			#10
+			Op = 6'b000100;
+			equal = 1'b1;
+			#10
+			Op = 6'b000100;
+			equal = 1'b1;
+	end
+ endmodule

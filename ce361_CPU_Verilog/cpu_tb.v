897f49a4e5be7e10fca538d88df79618f131448e
feistjo <joshua.feist@gmail.com>
2021-11-17 14:33:38 -0600

    t

--- ce361_CPU_Verilog/cpu_tb.v
+++ ce361_CPU_Verilog/cpu_tb.v
@@ -6,6 +6,7 @@ module cpu_tb();
 	
 	initial
 		begin
+			$monitor(test_cpu.datareg.mem_out, clk, Inst);
 			clk = 1'b1;
 			#1
 			Inst = 31'b000000_00000_00000_00001_00000_100000; //add $1, $0, $0

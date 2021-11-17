70541b18794ed50360a6a9ad607600575177f323
ChrisUustal <christopheruustal@gmail.com>
2021-11-16 21:30:15 -0600

    updated reg stuff more

--- ce361_CPU_Verilog/registers.v
+++ ce361_CPU_Verilog/registers.v
@@ -1,3 +1,49 @@
+//`include "lib/dff.v"
+//we use the DFF module and not DFFr because we want falling_edge(clk) function
+//putting this here for testing purposes
+module dff (clk, d, q);
+    input clk;
+    input d;
+    output reg q;
+    
+    always @(negedge clk)
+      begin
+         q <= d;
+      end
+      
+endmodule 
+
+module dff_32 (
+	clk,
+	d_32,
+	q_32
+	);
+	
+	//~~~~~~~~~~parameters~~~~~~~~~~
+	parameter DATA_WIDTH = 32;
+	
+	//~~~~~~~~~~inputs~~~~~~~~~~
+	input clk;
+	input [DATA_WIDTH-1:0] d_32;
+	
+	//~~~~~~~~~~outputs~~~~~~~~~~
+	output [DATA_WIDTH-1:0] q_32;
+	
+	//~~~~~~~~~~functionality~~~~~~~~~~
+	genvar i
+	generate 
+		for(i = 0; i < DATA_WIDTH; i=i+1) 
+		begin
+			dff cust_dff (
+				.clk 	(clk),
+				.d 		(d_32[i]),
+				.q 		(q_32[i])
+			);
+		end
+	endgenerate
+	
+endmodule
+
 module registers (
 	clk,
 	RegWr,
@@ -26,14 +72,31 @@ module registers (
 	output [DATA_WIDTH-1:0] busB;
 	
 	//~~~~~~~~~~Internal Variables~~~~~~~~~~
-	reg [DATA_WIDTH-1:0] mem [0:NUM_REGS-1];
+	wire [DATA_WIDTH-1:0] mem_in [0:NUM_REGS-1];
+	wire [DATA_WIDTH-1:0] mem_out [0:NUM_REGS-1];
+	
+	//generate our register array
+	genvar i;
+	generate 
+		for (i = 0; i < NUM_REGS; i = i+1) begin
+			dff_32 cust_dff_32 (
+				.clk 	(clk),
+				.d_32 	(mem_in[i]),
+				.q_32 	(mem_out[i])
+			);
+		end
+	endgenerate
 	
 	//~~~~~~~~~~Function~~~~~~~~~~
+	assign busA = mem_out[Ra];
+	assign busB = mem_out[Rb];
 	
-	//busA <= mem[Ra];
-	//busB <= mem[Rb];
-	//if(rising_edge(clk){
-	//	if(RegWr){mem[Rw] <= busW} end if;
-	//}
+	//if(RegWr) {mem[Rw] <= busW} 
+	mux_32 cust_mux_32 (
+		.sel 	(RegWr),
+		.src0 	(mem_out[Rw]),
+		.src1 	(busW),
+		.z 		(mem_in[Rw])
+	);
 
 endmodule

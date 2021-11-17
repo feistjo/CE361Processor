7343d02b98ff88a6f16c2665dedd817bdf3f27c6
feistjo <joshua.feist@gmail.com>
2021-11-17 00:18:38 -0600

    decode_func_tb

---
+++ ce361_CPU_Verilog/decode_func_tb.v
@@ -0,0 +1,15 @@
+ module decode_func_tb;
+	reg [5:0] Op, Fun;
+	wire [14:0] func;
+	wire RegDst;
+	
+	decode_func decode_func_t(Op, Fun, func, RegDst);
+	
+	initial
+		begin
+			$monitor(Op, Fun, func, RegDst);
+			Op = 6'b0;
+			Fun = 6'b100000
+			#10
+	end
+ endmodule

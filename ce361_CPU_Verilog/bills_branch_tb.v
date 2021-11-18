a426aabafb2ba40e45d5054ab96f8b6818875f52
feistjo <joshua.feist@gmail.com>
2021-11-18 01:39:50 -0600

    All test apps

---
+++ ce361_CPU_Verilog/bills_branch_tb.v
@@ -0,0 +1,22 @@
+module bills_branch_tb();
+	reg clk;
+	
+	cpu cpu1(clk);
+	
+	defparam cpu1.instmem.sram_get.mem_file = "data/bills_branch.dat";
+	defparam cpu1.datamem.syncram_1.mem_file = "data/bills_branch.dat";
+	
+	always
+	begin
+		clk = 1'b0;
+		#1;
+		clk = 1'b1;
+		#1;	
+	end
+	
+	initial
+		begin
+			#1000
+			$finish;
+	end
+endmodule

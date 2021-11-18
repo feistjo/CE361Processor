a5f465ce8dfee49da7cff3cd571f26967d516299
Christopher Robert Uustal <cru2515@batgirl.ece.northwestern.edu>
2021-11-17 21:26:32 -0600

    sup

--- ce361_CPU_Verilog/d_mem.v
+++ ce361_CPU_Verilog/d_mem.v
@@ -1,3 +1,5 @@
+//make sure to compile a syncram.v at some point before compiling this
+
 module d_mem(
     clk, //falling edge triggered
     data_in,
@@ -18,5 +20,16 @@ module d_mem(
 
     //~~~~~~~~~~Outputs~~~~~~~~~~
     output [DATA_WIDTH-1:0] data_out;
+
+    //~~~~~~~~~~Functions~~~~~~~~~~
+    syncram syncram_1 (
+        .clk (clk);
+        .cs (1'b1);
+        .oe (1'b1);
+        .we (WrEn);
+        .addr (adr);
+        .din (data_in);
+        .dout (data_out);
+    );
 
 endmodule

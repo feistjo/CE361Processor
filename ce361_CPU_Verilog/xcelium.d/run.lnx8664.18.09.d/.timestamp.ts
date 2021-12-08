11091fcf45392ec975780fce6975e57b60c3dd66
Lance Christopher Go <lancego2023@u.northwestern.edu>
2021-12-07 22:10:49 -0600

    fixed tb

--- ce361_CPU_Verilog/xcelium.d/run.lnx8664.18.09.d/.timestamp.ts
+++ ce361_CPU_Verilog/xcelium.d/run.lnx8664.18.09.d/.timestamp.ts
@@ -6,7 +6,7 @@
 1637019753 /home/lcg3895/ce361/CE361SingleCycleProcessor/ce361_CPU_Verilog/extend.v
 1637126062 /home/lcg3895/ce361/CE361SingleCycleProcessor/ce361_CPU_Verilog/lib/sram.v
 1637207662 /home/lcg3895/ce361/CE361SingleCycleProcessor/ce361_CPU_Verilog/inst_tb.v
-1638935864 /home/lcg3895/ce361/pipe/CE361Processor/ce361_CPU_Verilog/cpu_tb.v
+1638936485 /home/lcg3895/ce361/pipe/CE361Processor/ce361_CPU_Verilog/cpu_tb.v
 1637184457 /home/lcg3895/ce361/CE361SingleCycleProcessor/ce361_CPU_Verilog/pc_tb.v
 1637187218 /home/lcg3895/ce361/CE361SingleCycleProcessor/ce361_CPU_Verilog/branch_pc.v
 1637182747 /home/lcg3895/ce361/CE361SingleCycleProcessor/ce361_CPU_Verilog/registers.v
